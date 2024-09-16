2d0916eda28a8d7e372302e4fb2a27e9bee7baca
G-Mustafa02 <uzairmustafa963@gmail.com>
2024-09-16 02:42:58 -0700

    Add files via upload

---
+++ function/app.js
@@ -0,0 +1,23 @@
+// var currentdatetime = new Date
+//         var datetime = document.querySelector("#datetime");
+//         datetime.innerHTML = currentdatetime
+
+
+
+// var firstName = prompt("enter your first name")
+// var lastName = prompt("enter your last name")
+// var fullName = firstName + lastName
+
+// console.log("welcome" + fullName )
+
+
+
+
+
+// var num1 = prompt("enter your first num")
+// var num2 = prompt("eter your second number")
+// var total = +num1 + +num2;
+// console.log(total);
+
+
+
