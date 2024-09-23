5f68e9a3799e29f147f3d712c6c9f585f19088d5
G-Mustafa02 <uzairmustafa963@gmail.com>
2024-09-23 02:43:42 -0700

    Add files via upload

---
+++ if else statement assignment/New folder/app.js
@@ -0,0 +1,352 @@
+//MULTIPLE OF 3
+
+// var userNumber = prompt("enter your number");
+
+// if (userNumber % 3 == 0) {
+//     console.log("number is multiple of 3")
+// } else {
+//     console.log("number is not a multiple of 3");
+    
+// }
+
+
+
+
+
+//EVEN AND ODD
+// var userNumber = prompt("enter your number");
+
+// if (userNumber % 2 == 0) {
+//     console.log("number is even");
+    
+// } else if (userNumber % 3 == 0){
+//     console.log("number is odd"); 
+// }
+//  else {
+    
+// }
+
+
+
+// old and young
+// var userAge = prompt("enter your age");
+// if (userAge >= 18) {
+//     console.log("old enough");
+    
+// } else {
+//     console.log("too young");
+    
+// }
+
+
+
+
+// question 4
+// var userName = prompt("enter your name");
+// if (userName == "mustafa") {
+//     console.log("hello " + userName);
+    
+// } else {
+    
+// }
+
+
+
+// var userNumber = prompt("enter your number");
+
+// var reminder = userNumber % 3
+// switch (reminder) {
+//     case  0 :
+//         console.log("number is divided by 3");
+        
+//         break;
+
+//     default: console.log("not divided by 3");
+    
+//         break;
+// }
+
+
+
+
+// var input = prompt("enter your text")
+
+// if (input >= "0" && input <= "9") {
+//     console.log("this is a number");
+    
+// } else if (input.charCodeAt(0) >= "65" && input.charCodeAt(0) <= "90"){
+//     console.log("this is upper case letter");
+    
+// }else if (input.charCodeAt(0) >= "97" && input.charCodeAt(0) <= "122"){
+//     console.log("this is lower case letter");
+    
+// }
+
+
+
+
+
+
+// var num1 = prompt("enter your num 1")
+// var num2 = prompt("enter your num 2")
+// var op = prompt("enter your operator")
+
+// switch (op) {
+//     case '+':
+//         console.log(+num1 + +num2);
+        
+//         break;
+//     case '-':
+//         console.log(+num1 - +num2);
+
+//         break
+//     case '*':
+//         console.log(+num1 * +num2);
+//     break
+//     case '/':
+//         console.log(+num1 / +num2);
+//     break
+        
+//     default:
+//         break;
+// }
+
+
+// var time=prompt("time: ");
+// if(time>='0000' && time<'1200'){
+//     console.log("Good morning");
+// }
+// else if(time>='1200' && time<'1700'){
+//     console.log("Good afternoon");
+// }
+// else if(time>='1700' && time<'2100'){
+//     console.log("Good evening");
+// }
+// else if(time>='2100' && time<='2359'){
+//     console.log("Good night");
+// }
+
+
+
+
+
+
+
+
+// var year=Number(prompt("YYYY"))
+// if(year%4===0){
+//     if(year%100===0){
+//         if(year%400===0){
+//             console.log("leap year");
+//         }
+//         else{
+//             console.log("not a leap year");
+//         }
+//     }
+//     else{
+//         console.log(" leap year");
+//     }
+    
+// }
+// else{
+//     console.log("not a leap year");
+// }
+
+
+
+// var password = "mustafa"
+// var userpass = prompt("enter your password")
+// if (userpass === "") {
+//     console.log("enter your password");
+    
+// } else if (password == userpass){
+//     console.log("correct password");
+    
+// } else{console.log("incorrect password");
+// }
+
+
+
+
+
+//  var firstName = "Ali";
+//  if (firstName === "Fahad") {
+//   document.write("Hello Fahad!");
+//  }
+//  else{
+//      document.write("You are not Fahad")
+//  }
+
+
+
+
+// var greeting; 
+// var hour = 13; 
+// if (hour < 18) { 
+// greeting = "Good day"; 
+// console.log(greeting);
+
+// }else{ 
+// greeting = "Good evening"; 
+// console.log(greeting);
+
+// }
+
+
+
+// var num1=5
+// var num2=9
+// if(num1>num2){
+//     console.log(num1 + " is larger");
+// }
+// else if(num1<num2){
+//     console.log(num2 + " is larger");
+// }
+// else if(num1===num2){
+//     console.log("equal");
+// }
+
+
+
+
+// var num=Number(prompt("number: "));
+// if(num>0){
+//     console.log("positive");
+// }
+// else if(num<0){
+//     console.log("negaive");
+// }
+// else{
+//     console.log("zero");
+    
+// }
+
+
+
+
+// var variable=1;
+// if(typeof(variable)==="string"){
+//     console.log("string");
+// }
+// else if(typeof(variable)==="number"){
+//     console.log("number");
+// }
+// else if(typeof(variable)==="boolean"){
+//     console.log("boolean");
+// }
+// else{
+//     console.log("undefined");
+// }
+
+
+// var char="A"
+// char= char.toLowerCase();
+// if(char==="a" || char==="e" || char==="i" || char==="o" || char==="u"){
+//     console.log(true);   
+// }
+// else{
+//     console.log(false); 
+// }
+
+
+
+// console.log(10!==8);
+
+
+
+// var month=prompt("enter number of month");
+// switch (month) {
+//     case "1":
+//         alert("January")
+//         break;
+//     case "2":
+//         alert("February")
+//         break;
+//     case "3":
+//         alert("March")
+//         break;    
+//     case "4":
+//         alert("April")
+//         break;    
+//     case "5":
+//         alert("May")
+//         break;
+//     case "6":
+//         alert("June")
+//         break;
+//     case "7":
+//         alert("July")
+//         break;
+//     case "8":
+//         alert("August")
+//         break;
+//     case "9":
+//         alert("September")
+//         break;
+//     case "10":
+//         alert("October")
+//         break;
+//     case "11":
+//         alert("November")
+//         break;
+//     case "12":
+//         alert("December")
+//         break;
+
+//     default:
+//         alert("Invalid month")
+//         break;
+// }
+
+
+
+// var age=Number(prompt("age: "));
+// console.log( age<18? "too young": "old enough");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var num1 = document.querySelector("#num1")
+// var num2 = document.querySelector("#num2")
+// var calc = document.querySelector("#total")
+// function add(){
+//     var total = +num1.value + +num2.value
+//     calc.innerHTML = total
+// }
+
+// function sub(){
+//     var total = +num1.value - +num2.value
+//     calc.innerHTML = total
+// }
+// function multiply(){
+//     var total = +num1.value * +num2.value
+//     calc.innerHTML = total
+// }
+
+// function divide(){
+//     var total = +num1.value / +num2.value
+//     calc.innerHTML = total
+// }
+
+
+
+
+
+
+
+
+
+
